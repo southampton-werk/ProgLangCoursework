5b2194326257cdf6c4a0337531e4faad7edb478e
southamptonjake <jal2g15@soton.ac.uk>
2017-03-06 23:33:43 +0000

    with input now

--- main.ml
+++ main.ml
@@ -6,5 +6,5 @@ let _ =
     in  
        let result = Sdlparser.main Sdllexer.main lexbuf 
        in
-         prettyPrint result ; print_newline() ; flush stdout 
+         prettyPrint result "a" ; print_newline() ; flush stdout 
   with Parsing.Parse_error -> print_string "There was a problem parsing the SDL program. Please check your syntax. \n" ; flush stdout
--- path.ml
+++ path.ml
@@ -1,6 +1,6 @@
 (* Data type of terms *)
 type pTerm = Pred of string  ;;
 
-let rec prettyPrint pTerm = match pTerm with 
-  Pred (n) -> print_string "(Pred " ; print_string n ; print_string ")" 
+let rec prettyPrint pTerm input = match pTerm with 
+  Pred (n) -> print_string n ; print_string input
 ;; 
