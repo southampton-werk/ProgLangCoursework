a610a1462198a8196ebea0e1a94a3b7fb128ba32
southamptonjake <jal2g15@soton.ac.uk>
2017-03-07 15:36:59 +0000

    more than one line

--- main.ml
+++ main.ml
@@ -5,7 +5,7 @@ let _ =
     let lexbuf = Lexing.from_channel (open_in Sys.argv.(1))
     in
        let result = Sdlparser.main Sdllexer.main lexbuf 
-       and read = read_line()
+       and read = really_input_string stdin (in_channel_length stdin)
        in
          prettyPrint result read ; print_newline() ; flush stdout 
   with Parsing.Parse_error -> print_string "There was a problem parsing the SDL program. Please check your syntax. \n" ; flush stdout
