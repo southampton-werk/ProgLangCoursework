da3076df6ae644061e4c84100488d20c94e1a37f
southamptonjake <jal2g15@soton.ac.uk>
2017-03-07 15:22:51 +0000

    can now input

--- main.ml
+++ main.ml
@@ -4,7 +4,8 @@ let _ =
   try
     let lexbuf = Lexing.from_channel (open_in Sys.argv.(1))
     in
-       let result = Sdlparser.main Sdllexer.main lexbuf
+       let result = Sdlparser.main Sdllexer.main lexbuf 
+       and read = read_line()
        in
-         prettyPrint result "{a,a,b,c}" ; print_newline() ; flush stdout 
+         prettyPrint result read ; print_newline() ; flush stdout 
   with Parsing.Parse_error -> print_string "There was a problem parsing the SDL program. Please check your syntax. \n" ; flush stdout
