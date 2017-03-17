e3531c237fd726c9dc7f26e1993f1b7ab08b64b9
southamptonjake <jal2g15@soton.ac.uk>
2017-03-17 13:45:26 +0000

    now works with shitty linux proij

--- main.ml
+++ main.ml
@@ -1,4 +1,11 @@
 open Path
+let rec read_stdin acc  =
+  try
+    let line = "\n" ^ (input_line stdin) in
+      read_stdin (acc ^ line)
+  with
+    |End_of_file -> acc
+;;
 
 
 let _ =
@@ -6,7 +13,7 @@ let _ =
     let lexbuf = Lexing.from_channel (open_in Sys.argv.(1))
     in
        let result = Sdlparser.main Sdllexer.main lexbuf
-       and read = really_input_string stdin (in_channel_length stdin) in
+       and read = read_stdin "" in
         let lastLine = (Str.search_backward (Str.regexp "\n") read ((String.length read)-1))  in
           let k = Str.string_after read (lastLine + 1 )
           and input = Str.string_before read lastLine in
